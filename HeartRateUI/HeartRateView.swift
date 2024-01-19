e855fcfa1bd990282308953196235f653f5f35b9
Ajay <ajay.kumar@iphtechnologies.com>
2024-01-19 17:48:41 +0530

    add horizontal lines and date

--- HeartRateUI/HeartRateView.swift
+++ HeartRateUI/HeartRateView.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class HeartRateView: UIView {
-    let heartRateData = [60, 70, 80, 90, 100, 110, 120]
+    let heartRateData = [50, 60, 80, 90, 100, 90, 80]
     let daysOfWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
     let recentReadings = ["Today (14:00)", "Today (13:00)"]
     let recentValues = [125, 110]
@@ -33,24 +33,26 @@ class HeartRateView: UIView {
         let barX = margin
         let barY = height - graphHeight - margin
         
+        // vertical line
         for i in 0..<heartRateData.count - 1 {
             let x = barX + CGFloat(i + 1) * barWidth
             let linePath = UIBezierPath()
-            linePath.move(to: CGPoint(x: x, y: barY))
-            linePath.addLine(to: CGPoint(x: x, y: barY + barMaxHeight))
+            linePath.move(to: CGPoint(x: x, y: barY + 5))  // Adjust the value (30) as needed to reduce the height from the top
+            linePath.addLine(to: CGPoint(x: x, y: barY + barMaxHeight - 10))
             linePath.lineWidth = 1
-            UIColor.black.setStroke()
+            UIColor.lightGray.setStroke()
             linePath.stroke()
         }
+
         
         // Draw title
         let title = "Heart Rate"
-        let titleFont = UIFont.systemFont(ofSize: 24, weight: .bold)
+        let titleFont = UIFont.systemFont(ofSize: 25, weight: .bold)
         let titleAttributes = [NSAttributedString.Key.font: titleFont]
         let titleSize = title.size(withAttributes: titleAttributes)
         let titleX = (width - titleSize.width) / 2
         let titleY = margin
-        title.draw(at: CGPoint(x: titleX, y: titleY), withAttributes: titleAttributes)
+        title.draw(at: CGPoint(x: titleX - 100, y: titleY), withAttributes: titleAttributes)
         
         // Draw full stats
         let fullStats = "Full stats"
@@ -63,7 +65,7 @@ class HeartRateView: UIView {
         
         // Draw range
         let range = "\(bpmRange)\n\(dateRange)"
-        let rangeFont = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let rangeFont = UIFont.systemFont(ofSize: 20, weight: .bold)
         let rangeAttributes = [NSAttributedString.Key.font: rangeFont]
         let rangeSize = range.size(withAttributes: rangeAttributes)
         let rangeX = margin
@@ -71,33 +73,77 @@ class HeartRateView: UIView {
         range.draw(at: CGPoint(x: rangeX, y: rangeY), withAttributes: rangeAttributes)
         
         
+        let day = "Today (14:00)"
+        let dayFont = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let dayAttributes = [NSAttributedString.Key.font: dayFont]
+        let daySize = day.size(withAttributes: dayAttributes)
+        let dayX = margin
+        let dayY = titleY + titleSize.height + margin
+        day.draw(at: CGPoint(x: dayX, y: dayY + 520), withAttributes: dayAttributes)
+        
+        
+        let number1 = "125"
+        let number1Font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let number1Attributes = [NSAttributedString.Key.font: number1Font]
+        let number1Size = number1.size(withAttributes: number1Attributes)
+        let number1X = margin
+        let number1Y = titleY + titleSize.height + margin
+        number1.draw(at: CGPoint(x: number1X + 300, y: number1Y + 520), withAttributes: dayAttributes)
+        
+        let day1 = "Today (13:00)"
+        let day1Font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let day1Attributes = [NSAttributedString.Key.font: day1Font]
+        let day1Size = day.size(withAttributes: day1Attributes)
+        let day1X = margin
+        let day1Y = titleY + titleSize.height + margin
+        day1.draw(at: CGPoint(x: day1X, y: day1Y + 570), withAttributes: dayAttributes)
+        
+        let number2 = "140"
+        let day2Font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let day2Attributes = [NSAttributedString.Key.font: day1Font]
+        let day2Size = day.size(withAttributes: day2Attributes)
+        let day2X = margin
+        let day2Y = titleY + titleSize.height + margin
+        number2.draw(at: CGPoint(x: day2X + 300, y: day2Y + 570), withAttributes: dayAttributes)
+        
         // Draw graph
         let maxHeartRate = heartRateData.max()!
         let minHeartRate = heartRateData.min()!
         let avgY = barY + barMaxHeight - CGFloat(avgHeartRate - minHeartRate) / CGFloat(maxHeartRate - minHeartRate) * barMaxHeight
         let avgLine = UIBezierPath()
-        avgLine.move(to: CGPoint(x: barX, y: avgY))
-        avgLine.addLine(to: CGPoint(x: barX + graphWidth, y: avgY))
+        avgLine.move(to: CGPoint(x: barX, y: avgY + 100))
+        avgLine.addLine(to: CGPoint(x: barX + graphWidth + 500, y: avgY + 100))
         avgLine.lineWidth = 1
         UIColor.black.setStroke()
         avgLine.stroke()
         
+        let avgYx = barY + barMaxHeight - CGFloat(avgHeartRate - minHeartRate) / CGFloat(maxHeartRate - minHeartRate) * barMaxHeight
+        let avgLinex = UIBezierPath()
+        avgLine.move(to: CGPoint(x: barX, y: avgY + 155))
+        avgLine.addLine(to: CGPoint(x: barX + graphWidth + 500, y: avgY + 155))
+        avgLine.lineWidth = 1
+        UIColor.black.setStroke()
+        avgLine.stroke()
         
         
         for i in 0..<heartRateData.count {
             let x = barX + CGFloat(i) * barWidth
             let y = barY + barMaxHeight - CGFloat(heartRateData[i] - minHeartRate) / CGFloat(maxHeartRate - minHeartRate) * barMaxHeight
 
-            // Draw rounded rectangle bar
-            let bar = UIBezierPath(roundedRect: CGRect(x: x + barMargin, y: y, width: barWidth - barMargin * 2, height: barMaxHeight - y + barY), cornerRadius: 12.0)
-            UIColor.red.setFill()
-            bar.fill()
+            // Convert hex color to UIColor
+            let hexColor = 0xfa576c  // Replace with your hex color value
+            let uiColor = UIColor(
+                red: CGFloat((hexColor & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((hexColor & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(hexColor & 0x0000FF) / 255.0,
+                alpha: 1.0
+            )
 
-            // Draw dot with rounded corners
-//            let dotRadius: CGFloat = 1.0
-//            let dot = UIBezierPath(roundedRect: CGRect(x: x + barWidth / 2 - dotRadius, y: y - dotRadius, width: dotRadius * 2, height: dotRadius * 2), cornerRadius: dotRadius)
-            UIColor.black.setFill()
-      //      dot.fill()
+            // Draw rounded rectangle bar with hex color
+            let narrowerBarWidth = barWidth * 0.7 // Adjust the factor (0.8) as needed
+            let bar = UIBezierPath(roundedRect: CGRect(x: x + (barWidth - narrowerBarWidth) / 2, y: y - 70, width: narrowerBarWidth, height: barMaxHeight - y + barY), cornerRadius: 20.0)
+            uiColor.setFill()
+            bar.fill()
 
             // Draw day of the week label
             let dayOfWeek = daysOfWeek[i]
@@ -106,7 +152,9 @@ class HeartRateView: UIView {
             let dayOfWeekSize = dayOfWeek.size(withAttributes: dayOfWeekAttributes)
             let dayOfWeekX = x + (barWidth - dayOfWeekSize.width) / 2
             let dayOfWeekY = barY + barMaxHeight
+            
         }
+
 
     }
     
