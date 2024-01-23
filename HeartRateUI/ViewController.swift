5b685e65442935a9e6ec7e58e958c226a6e61dce
Ajay <ajay.kumar@iphtechnologies.com>
2024-01-23 14:50:14 +0530

    average and horizontal line added

--- HeartRateUI/ViewController.swift
+++ HeartRateUI/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var graphView: UIView!
     @IBOutlet weak var leftBottomView: UIView!
     @IBOutlet weak var rightBottomView: UIView!
+    @IBOutlet weak var avgButton: UIButton!
     
     
     
@@ -22,10 +23,39 @@ class ViewController: UIViewController {
        
         leftBottomView.layer.cornerRadius = 10
         rightBottomView.layer.cornerRadius = 10
+        
+        avgButton.clipsToBounds = true
+        applyCornerRadiusToRightSide(of: avgButton, cornerRadius: 20)
+        
+        
+        addHorizontalLineToAvgButton()
+        
     }
     
-    
-    
-    
+    func addHorizontalLineToAvgButton() {
+        
+           let lineLayer = CALayer()
+           lineLayer.backgroundColor = UIColor.darkGray.cgColor
+           let lineWidth: CGFloat = 1.0
+           let lineY = avgButton.center.y
+           let lineX = avgButton.frame.origin.x + avgButton.frame.size.width / 2.0 + 40
+           lineLayer.frame = CGRect(x: lineX, y: lineY, width: avgButton.frame.size.width + 400, height: lineWidth)
+           graphView.layer.addSublayer(lineLayer)
+        
+       }
+       
+       func applyCornerRadiusToRightSide(of view: UIView, cornerRadius: CGFloat) {
+           
+           let maskPath = UIBezierPath(
+               roundedRect: view.bounds,
+               byRoundingCorners: [.topRight, .bottomRight],
+               cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)
+           )
+           let maskLayer = CAShapeLayer()
+           maskLayer.path = maskPath.cgPath
+           view.layer.mask = maskLayer
+           
+       }
+
 
 }
